8c4f284856e2d992e277abd549a5cc997fadcacd
Aurelian2025 <aureliang@iceworksbathco.ca>
2025-11-26 09:00:28 -0500

    Update Tub3D.tsx

--- app/Tub3D.tsx
+++ app/Tub3D.tsx
@@ -23,6 +23,7 @@ const PLATE_SAMPLE_POINTS: { u: number; v: number }[] = [
   { u: 1 / 3, v: 0.20 },
   { u: 2 / 3, v: 0.20 },
   { u: 0.95, v: 0.20 }
+  { u: 0.50, v: 0.50 }
 ];
 const SHORT_SAMPLE_POINTS_5: { u: number; v: number }[] = [
   { u: 0.05, v: 0.10 },
